88733c0925baf8549c0b4add1be866a8a4476125
troypeirce <36358345+troypeirce@users.noreply.github.com>
2026-07-13 23:30:51 -0600

    Publish js/photos.js

---
+++ js/photos.js
@@ -0,0 +1,12 @@
+window.ROOTS_LANDING_ALBUMS = {
+  "March 2022": [
+    "images/March 2022/IMG_0571-a6c62849c8ee.jpeg",
+    "images/March 2022/IMG_0572-d4e17fd73e25.jpeg"
+  ],
+  "November 2022": [
+    "images/November 2022/IMG_2943-ce37d6930225.jpeg"
+  ],
+  "Date Unknown": [
+    "images/Date Unknown/IMG_1412-ba689af514b9.png"
+  ]
+};
